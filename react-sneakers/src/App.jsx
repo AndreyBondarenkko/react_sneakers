5771720cd7071afa7b67b35265fd0166c74e391f
Andrey Bondarenko <andrey.bondarenko.devmrks@gmail.com>
2023-01-26 23:56:48 +0200

    onClick / cssModule

--- react-sneakers/src/App.jsx
+++ react-sneakers/src/App.jsx
@@ -6,6 +6,10 @@ import Header from './components/Header/Header';
 import Drawer from './components/Drawer/Drawer';
 import Card from './components/Card/Card';
 
+
+/*** Data object ***/
+const data = [{"key":1,"title":"Nike Pegasus Trail 3","description":"Mens Trail Running Shoes","price":170,"imageUrl":"./img/sneakers/1.jpg"},{"key":2,"title":"Nike Pegasus Trail 3 GORE-TEX","description":"Mens Waterproof Trail Running Shoes","price":210,"imageUrl":"./img/sneakers/2.jpg"},{"key":3,"title":"Nike React Miler 2 Shield","description":"Mens Weatherised Road Running Shoes","price":126,"imageUrl":"./img/sneakers/3.jpg"},{"key":4,"title":"Nike Free Run 5.0","description":"Mens Road Running Shoes","price":135,"imageUrl":"./img/sneakers/4.jpg"},{"key":5,"title":"Nike React Miler 2 Shield","description":"Men's Weatherised Road Running Shoes","price":180,"imageUrl":"./img/sneakers/5.jpg"},{"key":6,"title":"Мужские Кроссовки Nike Blazer Mid Suede","price":12999,"imageUrl":"img/sneakers/6.jpg"},{"key":7,"title":"Кроссовки Black Edition","price":16999,"imageUrl":"img/sneakers/6.jpg"},{"key":8,"title":"Кроссовки Orange Boomb Edition","price":7499,"imageUrl":"img/sneakers/7.jpg"}];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -28,7 +32,17 @@ function App() {
 
         { /*** Cards ***/ }
         <div className="card-grid d-flex flex-wrap">
-          <Card />
+          {
+            data.map(product => 
+            <Card 
+              id={product.key}
+              title={product.title}
+              description={product.description}
+              image={product.imageUrl}
+              price={product.price}
+              click={()=>console.log(product)}
+            />)
+          }
         </div>
         
       </div>  
--- react-sneakers/src/components/Card/Card.jsx
+++ react-sneakers/src/components/Card/Card.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({id, title, description, price, image, click}) => {
+    const buttonClick = () =>{
+        alert(price)
+    } 
   return (
-    <div className="card">
+    <div id={id} className="card">
         <div className="favorite" >
             <img src='img/unliked.svg' alt="Unliked" />
         </div>
         <div className="card-image-product d-flex justify-center">
-            <img width="auto" height={135} src="./img/sneakers/1.jpg"  alt="product"/>
+            <img width="auto" height={135} src={image}  alt={title}/>
         </div>
         <div className="card-info">
-        <h5>Card</h5>
-        <div className="description-product">Мужские кроссовки Nike Blazer Suende</div>
+        <h5>{title}</h5>
+        <div className="description-product">{description}</div>
         <div className="d-flex justify-between align-center">
             <div className="d-flex flex-column">
                 <span>Цена:</span>
-                <b>12 999 $</b>
+                <b>{price} $</b>
             </div>
-            <div>
+            <div onClick={ click }>
                 <img className='plus' src='img/btn-plus.svg' alt="Plus"/>
             </div>
         </div>
